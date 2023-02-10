03465c6904d967b73e5816fb8b715b2eb948d7fd
Mickael Gonzales <mickael.gonzales@hotmail.com>
2023-02-10 15:44:08 +0100

    Ajout inscription et authentification user

---
+++ src/components/LogForm/LogForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import styles from "./LogForm.module.css";
+
+
+
+
+export default function SignForm() {
+
+    const[user, setUser] = useState({
+        email:"",
+        password:""
+    })
+
+    const handleChange = (e) =>{
+        setUser({...user, [e.target.name]: e.target.value})     //spread user pour ne pas perdre autres données lors de update
+    }
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault();
+        await fetch('/api/createUser',{method: "POST", body: {}})
+        setUser({email:"", password:""})              //equivalent de input.value="" après enregistrer formulaire
+    }
+
+    return (
+      <form className={styles.form} action="/api/logIn" method="post">
+        <input type="email" name="email" placeholder="Email" value={user.email} onChange={handleChange} />
+        <input type="password" name="password" placeholder="Mot de Passe" autoComplete="new-password" value={user.password} onChange={handleChange} />
+        <button type="submit">Se connecter</button>
+      </form>
+    );
+  }
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+import styles from "./Title.module.css";
+
+
+export default function Title() {
+  return (
+    <>
+      <h1 className={styles.title}>MyPeacefulPlace</h1>
+    </>
+  );
+}
