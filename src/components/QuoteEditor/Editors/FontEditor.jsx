aa4e0c04aa9300d74c214757d46f1fd56c78cfb1
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-02-24 11:15:06 +0100

    add fonts to editor

--- src/components/QuoteEditor/Editors/FontEditor.jsx
+++ src/components/QuoteEditor/Editors/FontEditor.jsx
@@ -1,6 +1,54 @@
 import styles from "./Editor.module.css";
+import { Courgette } from '@next/font/google';
+import { Merriweather } from '@next/font/google';
+import { Caveat } from '@next/font/google';
+import { Zeyada } from '@next/font/google';
+import { Great_Vibes } from '@next/font/google';
+
+
+const courgette = Courgette({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+const GreatVibes = Great_Vibes({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+const zeyada = Zeyada({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+const caveat = Caveat({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+//Faire un tableau de polices et mapper pour créer les li
 
 export default function FontEditor() {
+
+  function setPolice(event) {
+    //Je souhaite récupérer le nom de la classe de la li cliquée et l'ajouter à ma div de texte test
+    
+    const clickedPolice = event.target.classList[0];
+   
+    const textDiv = document.querySelector(`.${styles.testFontText}`)
+    // console.log(textDiv)
+
+    const textDivClass = textDiv.classList[1];
+    console.log(clickedPolice)
+
+    textDiv.classList.replace(textDivClass, `${clickedPolice}`);
+  }
+
   return (
     <div
       style={{
@@ -9,6 +57,15 @@ export default function FontEditor() {
       }}
       className={styles.editor}
     >
+        <div className={`${styles.testFontText} ${caveat.className}`} id="test-font">
+          Le cheval c'est génial
+        </div>
+        <ul>
+          <li  className={`${zeyada.className} ${styles.policeItem}`} onClick={setPolice}>Il est temps de rallumer les étoiles</li>
+          <li  className={`${merriweather.className} ${styles.policeItem}`} onClick={setPolice}>Il est temps de rallumer les étoiles</li>
+          <li  className={`${caveat.className} ${styles.policeItem}`} onClick={setPolice}>Il est temps de rallumer les étoiles</li>
+          <li  className={`${GreatVibes.className} ${styles.policeItem}`} onClick={setPolice}>Il est temps de rallumer les étoiles</li>
+        </ul>
       FontEditor
     </div>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { Courgette } from '@next/font/google'
+
+
+const courgette = Courgette({
+  subsets: ['latin'],
+  weight: '400'
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+  <main>
+    <Component {...pageProps} />
+  </main>
+)}
