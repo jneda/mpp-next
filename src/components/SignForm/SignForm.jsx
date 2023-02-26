46e79589b4f3b5943a2d52d71c5c5d4301292b12
jneda <julien.dane@gmail.com>
2023-02-26 17:50:50 +0100

    try and log newly registered user

--- src/components/SignForm/SignForm.jsx
+++ src/components/SignForm/SignForm.jsx
@@ -1,6 +1,8 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 import styles from "./SignForm.module.css";
 export default function SignForm() {
+    const router = useRouter();
 
     const[user, setUser] = useState({
         name:"",
@@ -14,12 +16,24 @@ export default function SignForm() {
 
     const handleSubmit = async(e) =>{
         e.preventDefault();
-        await fetch('/api/createUser', {method: "POST", body: {}})
+        let response;
+        try {
+          response = await fetch('/api/createUser', {method: "POST", body: JSON.stringify(user)});
+        } catch (err) {
+          console.error(err);
+        }
+        console.log("[SignForm component]", response);
+        if (!response.ok) {
+          // handle error
+          return;
+        }
         setUser({name: "", email:"", password:""})              //equivalent de input.value="" après enregistrer formulaire
+        
+        return router.push("/homepage");
     }
 
     return (
-      <form className={styles.form} action="/api/createUser" method="post">
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Pseudo" value={user.name} onChange={handleChange} />
         <input type="email" name="email" placeholder="Email" value={user.email} onChange={handleChange} />
         <input type="password" name="password" placeholder="Mot de Passe" autoComplete="new-password" value={user.password} onChange={handleChange} />
