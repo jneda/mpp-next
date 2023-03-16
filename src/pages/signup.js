ff46795be06b09dfc000d6c1f01f010e310ac10e
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-03-16 09:42:37 +0100

    add color logo to signup and login pages

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,13 +1,14 @@
 import Title from "../components/Title/Title.jsx";
 import Logo from "../components/Logo/Logo.jsx";
 import SignForm from "@/components/SignForm/SignForm.jsx";
+import LogoColor from "@/components/LogoColor/LogoColor.jsx";
 
 export default function() {
 
     return (
         <>
             <Title />
-            <Logo />
+            <LogoColor />
             <SignForm />
         </>
     );
